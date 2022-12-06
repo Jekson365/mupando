c5a0ed5fe8d906c5991dfe1dcb2f11321ffe73bd
dadsasdl <jemali.ergemlidze.1@btu.edu.ge>
2022-12-06 23:17:04 +0400

    cc

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import './styles/main.scss'
+
+function App() {
+  return (
+    <>
+    </>
+  
+  )
+}
+
+export default App
